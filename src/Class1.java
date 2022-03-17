bd9cf9327f89df96cd6b82bb82b2df24874c75ab
Evan Yip <evanyip05@gmail.com>
2022-03-17 11:19:25 -0700

    Master change

---
+++ src/Class1.java
@@ -0,0 +1,13 @@
+public class Class1 {
+
+    private String string;
+
+    public Class1(String s) {
+        string = s;
+    }
+
+    @Override
+    public String toString() {
+        return "this is class 1";
+    }
+}
